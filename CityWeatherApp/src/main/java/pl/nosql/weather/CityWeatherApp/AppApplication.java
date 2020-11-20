d1192e5fe6b4fea44aeafa8f9a66a6a6bac309fc
pantczak <piotrmateuszantczak@protonmail.com>
2020-11-20 14:29:01 +0100

    added Spring Boot app base

---
+++ CityWeatherApp/src/main/java/pl/nosql/weather/CityWeatherApp/AppApplication.java
@@ -0,0 +1,13 @@
+package pl.nosql.weather.CityWeatherApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
---
+++ CityWeatherApp/src/test/java/pl/nosql/weather/CityWeatherApp/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.nosql.weather.CityWeatherApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
