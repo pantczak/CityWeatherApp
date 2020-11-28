94c8daa1a51cfcf6adef4386c89afa62242fa6ec
pantczak <piotrmateuszantczak@protonmail.com>
2020-11-28 19:55:39 +0100

    added CassandraConfig class

---
+++ CityWeatherApp/src/main/java/pl/nosql/weather/cityweatherapp/config/CassandraConfig.java
@@ -0,0 +1,73 @@
+package pl.nosql.weather.cityweatherapp.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
+import org.springframework.data.cassandra.core.cql.keyspace.DropKeyspaceSpecification;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+import java.util.List;
+
+@Configuration
+@EnableCassandraRepositories
+public class CassandraConfig extends AbstractCassandraConfiguration {
+
+    @Value("${spring.data.cassandra.keyspace-name}")
+    private String keyspace;
+
+    @Value("${spring.data.cassandra.port}")
+    private int port;
+
+    @Value("${spring.data.cassandra.contact-points}")
+    private String contactPoints;
+
+    @Value("${spring.data.cassandra.basepackages}")
+    private String basePackages;
+
+    @Value("${spring.data.cassandra.local-datacenter}")
+    private String dataCenter;
+
+    @Override
+    protected String getLocalDataCenter() {
+        return dataCenter;
+    }
+
+    @Override
+    public SchemaAction getSchemaAction() {
+        return SchemaAction.CREATE_IF_NOT_EXISTS;
+    }
+
+    @Override
+    public String[] getEntityBasePackages() {
+        return new String[]{basePackages};
+    }
+
+    @Override
+    protected String getContactPoints() {
+        return contactPoints;
+    }
+
+    @Override
+    protected int getPort() {
+        return port;
+    }
+
+    @Override
+    protected String getKeyspaceName() {
+        return keyspace;
+    }
+
+    @Override
+    protected List<CreateKeyspaceSpecification> getKeyspaceCreations() {
+        final CreateKeyspaceSpecification specification = CreateKeyspaceSpecification.createKeyspace(keyspace)
+                .ifNotExists().withSimpleReplication();
+        return List.of(specification);
+    }
+
+    @Override
+    protected List<DropKeyspaceSpecification> getKeyspaceDrops() {
+        return List.of(DropKeyspaceSpecification.dropKeyspace(keyspace));
+    }
+}
