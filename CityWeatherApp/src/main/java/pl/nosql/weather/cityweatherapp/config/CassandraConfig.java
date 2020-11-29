1f82b952b4d89f37b673bd7ac7e4803aaf18b6e8
pantczak <piotrmateuszantczak@protonmail.com>
2020-11-29 13:32:58 +0100

    redone dockerfile

--- CityWeatherApp/src/main/java/pl/nosql/weather/cityweatherapp/config/CassandraConfig.java
+++ CityWeatherApp/src/main/java/pl/nosql/weather/cityweatherapp/config/CassandraConfig.java
@@ -1,9 +1,9 @@
 package pl.nosql.weather.cityweatherapp.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
-import org.springframework.data.cassandra.config.SchemaAction;
+import org.springframework.data.cassandra.config.*;
 import org.springframework.data.cassandra.core.cql.keyspace.CreateKeyspaceSpecification;
 import org.springframework.data.cassandra.core.cql.keyspace.DropKeyspaceSpecification;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
@@ -13,6 +13,12 @@ import java.util.List;
 @Configuration
 @EnableCassandraRepositories
 public class CassandraConfig extends AbstractCassandraConfiguration {
+
+    @Value("${spring.data.cassandra.username}")
+    private String username;
+
+    @Value("${spring.data.cassandra.password}")
+    private String password;
 
     @Value("${spring.data.cassandra.keyspace-name}")
     private String keyspace;
