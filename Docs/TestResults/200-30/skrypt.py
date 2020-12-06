77112dcb2b624bf0be73358eae03b1f5c8e4009a
224406 Pietrzak Witold <224406@edu.p.lodz.pl>
2020-12-06 23:10:44 +0100

    Added tests

---
+++ Docs/TestResults/200-30/skrypt.py
@@ -0,0 +1,25 @@
+import csv
+
+with open('Raport.csv', newline='') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+    csvfile.readline()
+    time = 0.0
+    CPU = 0.0
+    Memory = 0.0
+    IO = 0.0
+    counter = 0.0
+    for row in spamreader:
+        y = str(row)
+        y = y[:-2]
+        x = y.split(',')
+        counter += 1
+        CPU += float(x[1])
+        Memory += float(x[2])
+        IO += float(x[3])
+    time /= counter
+    CPU /= counter
+    Memory /= counter
+    IO /= counter
+    print("CPU: "+str(CPU))
+    print("Pamięć: "+str(Memory))
+    print("I/O: "+str(IO))
